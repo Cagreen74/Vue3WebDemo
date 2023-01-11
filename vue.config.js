52ffb0e369e6303e55781ad9c5803cf267377928
Cagreen74 <Cgreen7414@gmail.com>
2023-01-11 16:23:40 -0600

    config
    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "https://thankful-sky-0195c9410.2.azurestaticapps.net/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
 };
 
